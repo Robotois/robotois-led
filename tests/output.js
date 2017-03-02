6b2a128c0de5c7330944ca74024dbe1a7805d507
Armando Gonzalez <iam@armando.mx>
2017-03-02 14:28:39 -0600

    linter support for tests

--- tests/output.js
+++ tests/output.js
@@ -1,15 +1,15 @@
-var _led = require('../index'),
-  led = new _led(5);
+const LED = require('../index');
 
+const led = new LED(5);
 
-setInterval(()=>{ // Proceso en estado ocioso
-  led.switch();
-},1000);
+setInterval(() => { // Proceso en estado ocioso
+  led.toggle();
+}, 1000);
 
-process.on('SIGTERM', function () {
+process.on('SIGTERM', () => {
   process.exit();
 });
 
-process.on('SIGINT', function () {
+process.on('SIGINT', () => {
   process.exit();
 });
